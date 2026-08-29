53ce74ae793300730f1ac728a9811030852d6bb4
YidanPan <panyidan0109@gmail.com>
2026-08-29 15:53:38 +0800

    检索优化：使用mmr方式检索

--- retriever.py
+++ retriever.py
@@ -1,9 +1,11 @@
 from config import RETRIEVAL_K
 
-# 从存储的向量中检索
+# 从存储的向量中检索 通过mmr方式检索，提高相关性
 def get_retriever(vectorstore):
     return vectorstore.as_retriever(
+        search_type="mmr",
         search_kwargs={
-            "k": RETRIEVAL_K
+            "k": 3,
+            "fetch_k": 10 #从候选10个中选出3个
         }
     )
